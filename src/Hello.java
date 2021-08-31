254e419060ee6a68dacdaf680cbbe9c8d5beffdd
ARElton <ameliareneeelton@gmail.com>
2021-08-31 14:36:58 -0400

    add variable

--- src/Hello.java
+++ src/Hello.java
@@ -3,6 +3,8 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, Amelia");
 
-        int myFirstNumber = 5;
+        int myFirstNumber = (10 + 5) + (2 * 10);
+
+        System.out.println(myFirstNumber);
     }
 }
